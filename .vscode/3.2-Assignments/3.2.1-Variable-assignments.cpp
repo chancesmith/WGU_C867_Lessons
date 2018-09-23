c4a5367cc40347f44f7ad1a41b45285ddc9a4cd9
Chance Smith <chancesmithb@gmail.com>
2018-09-23 15:20:05 -0500

    📦  NEW: adds 3.2 exercises

---
+++ .vscode/3.2-Assignments/3.2.1-Variable-assignments.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int x = 0;
+  int y = 4;
+
+  x = 9;
+
+  cout << x << " " << y;
+
+  return 0;
+}
---
+++ .vscode/3.2-Assignments/3.2.5-Variable-assignments.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int w = -2;
+  int x = -7;
+  int y = -8;
+  int z;
+
+  z = x - y;
+  z = z * w;
+  z = z / w;
+
+  cout << z << endl;
+}
