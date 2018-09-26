996597e539aab00fd82087dcb38fd287c59df319
Chance Smith <chancesmithb@gmail.com>
2018-09-26 06:44:07 -0500

    ⚡  IMPROVE: adds 7.19 header file guard

--- 7-19_Seperate-Files/threeintsfcts/threeintsfcts.h
+++ 7-19_Seperate-Files/threeintsfcts/threeintsfcts.h
@@ -1,2 +1,8 @@
+// adds Header file guard
+#ifndef THREEINTSFCT_H
+#define THREEINTSFCT_H
+
 int ThreeIntsSum(int num1, int num2, int num3);
 int ThreeIntsAvg(int num1, int num2, int num3);
+
+#endif
